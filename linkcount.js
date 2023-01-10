0ea098f55de3bf173b032746e3f7e3e70cdc9f45
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2023-01-10 15:17:16 -0800

    date fix

--- linkcount.js
+++ linkcount.js
@@ -8,13 +8,47 @@ window.onload=function(){
 
     var form = document.getElementById("EmployeeID");
     form.addEventListener("submit",function(event){
+        const d = new Date();
+        let day = d.getDay();
         event.preventDefault();
         username = form.username.value;
+        console.log(day);
         var inputs = document.getElementById("Employee");
         inputs.innerHTML = "Employee Number:" + username;
-        
+        /*
         var appear = document.getElementById("disappear");
         appear.style.display="block";
+        */
+        var mondayAppear = document.getElementsByClassName("1");
+        var tuesdayAppear = document.getElementsByClassName("2");
+        var wednesdayAppear = document.getElementsByClassName("3");
+        var thursdayAppear = document.getElementsByClassName("4");
+        var countArray = mondayAppear.length;
+        for(var i = 0; i < countArray; i++){
+            if(day==1){
+                mondayAppear[i].style.display="block";
+                tuesdayAppear[i].style.display="none";
+                wednesdayAppear[i].style.display="none";
+                thursdayAppear[i].style.display="none";
+            }else if(day==2){
+                mondayAppear[i].style.display="none";
+                tuesdayAppear[i].style.display="block";
+                wednesdayAppear[i].style.display="none";
+                thursdayAppear[i].style.display="none";
+            }else if(day==3){
+                mondayAppear[i].style.display="none";
+                tuesdayAppear[i].style.display="none";
+                wednesdayAppear[i].style.display="block";
+                thursdayAppear[i].style.display="none";
+            }else{
+                mondayAppear[i].style.display="none";
+                tuesdayAppear[i].style.display="none";
+                wednesdayAppear[i].style.display="none";
+                thursdayAppear[i].style.display="block";
+            }
+        }
+            
+
         /*
         var tab = document.createElement("table");
         tab.setAttribute("class","links");
